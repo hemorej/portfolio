5a781c7afb935912cbffeb73ce7b6b87fc842866
j_a_ <jerome.arfouche@gmail.com>
2019-11-19 18:42:56 -0500

    update side links

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,9 +9,7 @@
     </div>
     <div class="medium-4 small-12 columns">
       <ul class="no-bullet side">
-        <li><a href="http://twitter.com/jerome_a_" target=_blank>twitter/jerome_a_</a></li>
         <li><a href="http://instagram.com/jerome_a_" target=_blank>instagram/jerome_a_</a></li>
-        <li><a href="http://flickr.com/photos/jerome88" target=_blank>flickr/jerome88</a></li>
         <li><a href="http://codepen.io/jerome_a_/" target=_blank>codepen/jerome_a_</a></li>
         <li><a href="http://ello.co/hemorej/" target=_blank>ello/hemorej</a></li>
         <li><a href="http://github.com/hemorej/" target=_blank>github/hemorej</a></li>
