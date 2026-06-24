db1a7053aa4795e84fdb44f35a791c065b2af2d9
j_a_ <jerome.arfouche@pm.me>
2026-06-24 19:35:44 -0400

    refactoring
    - use cdn for fonts
    - remove bin files from repo, use symlinks on npm deploy
    - restore forge_deploy

---
+++ scripts/forge_deploy.sh
@@ -0,0 +1,13 @@
+cd /tmp
+
+rm -rf portfolio
+git clone --depth 1 --branch=$FORGE_SITE_BRANCH --recurse-submodules=false $REPO_URL 
+cd portfolio
+rsync -a --remove-source-files static/* $FORGE_SITE_PATH
+
+# if [ ! -e $FORGE_SITE_PATH/assets/images ]; then
+#     cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/images .
+# fi
+# if [ ! -e $FORGE_SITE_PATH/assets/font ]; then
+#     cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/font .
+# fi
--- scripts/prepare-deploy.js
+++ scripts/prepare-deploy.js
@@ -4,22 +4,15 @@ const path = require("path");
 const SRC = path.join(__dirname, "..");
 const DIST = path.join(__dirname, "../static");
 
-function copyDir(src, dest) {
-  fs.mkdirSync(dest, { recursive: true });
-  for (const entry of fs.readdirSync(src, { withFileTypes: true }).filter(e => !e.name.startsWith("."))) {
-    fs.copyFileSync(
-      path.join(src, entry.name),
-      path.join(dest, entry.name)
-    );
-  }
-}
 
 fs.rmSync(DIST, { recursive: true, force: true });
 fs.mkdirSync(DIST);
 
+const VOLUME = "/mnt/volume_tor1_01";
+
 fs.copyFileSync(path.join(SRC, "index.html"), path.join(DIST, "index.html"));
-fs.copyFileSync(path.join(SRC, "resume.pdf"), path.join(DIST, "resume.pdf"));
-copyDir(path.join(SRC, "ico"), path.join(DIST, "ico"));
-copyDir(path.join(SRC, "fonts"), path.join(DIST, "fonts"));
+fs.symlinkSync(path.join(VOLUME, "resume.pdf"), path.join(DIST, "resume.pdf"));
+fs.symlinkSync(path.join(VOLUME, "ico"), path.join(DIST, "ico"));
+fs.symlinkSync(path.join(VOLUME, "fonts"), path.join(DIST, "fonts"));
 
 console.log("static/ ready");
