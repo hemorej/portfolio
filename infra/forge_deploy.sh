409035a78e8750751897d60a543952c1acea60bf
j_a_ <jerome.arfouche@pm.me>
2026-06-26 14:11:04 -0400

    deploy script update

--- infra/forge_deploy.sh
+++ infra/forge_deploy.sh
@@ -5,12 +5,9 @@ git clone --depth 1 --branch=$FORGE_SITE_BRANCH --recurse-submodules=false $REPO
 cd portfolio
 rsync -a --remove-source-files static/* $FORGE_SITE_PATH
 
-if [ ! -e $FORGE_SITE_PATH/assets/images ]; then
-    cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/images .
-fi
-if [ ! -e $FORGE_SITE_PATH/assets/font ]; then
-    cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/font .
-fi
-if [ ! -e $FORGE_SITE_PATH/media ]; then
-    cd $FORGE_SITE_PATH && ln -s /mnt/$VOLUME_NAME/media .
-fi
+# if [ ! -e $FORGE_SITE_PATH/assets/images ]; then
+#     cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/images .
+# fi
+# if [ ! -e $FORGE_SITE_PATH/assets/font ]; then
+#     cd $FORGE_SITE_PATH/assets && ln -s /mnt/$VOLUME_NAME/assets/font .
+# fi
