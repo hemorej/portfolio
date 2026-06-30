fde1e8a7fd84d840562e1bf43b3005ff369e99c0
j_a_ <jerome.arfouche@pm.me>
2026-06-30 16:02:42 -0400

    bundle perf improvements

--- scripts/prepare-deploy.js
+++ scripts/prepare-deploy.js
@@ -1,18 +1,30 @@
 const fs = require("fs");
 const path = require("path");
+const { minify } = require("html-minifier-terser");
 
 const SRC = path.join(__dirname, "..");
 const DIST = path.join(__dirname, "../static");
 
+async function main() {
+  fs.rmSync(DIST, { recursive: true, force: true });
+  fs.mkdirSync(DIST);
 
-fs.rmSync(DIST, { recursive: true, force: true });
-fs.mkdirSync(DIST);
+  const VOLUME = "/mnt/volume_tor1_01";
 
-const VOLUME = "/mnt/volume_tor1_01";
+  const html = fs.readFileSync(path.join(SRC, "index.html"), "utf8");
+  const minified = await minify(html, {
+    collapseWhitespace: true,
+    removeComments: true,
+    minifyCSS: true,
+    minifyJS: true,
+  });
+  fs.writeFileSync(path.join(DIST, "index.html"), minified);
 
-fs.copyFileSync(path.join(SRC, "index.html"), path.join(DIST, "index.html"));
-fs.symlinkSync(path.join(VOLUME, "resume.pdf"), path.join(DIST, "resume.pdf"));
-fs.symlinkSync(path.join(VOLUME, "ico"), path.join(DIST, "ico"));
-fs.symlinkSync(path.join(VOLUME, "fonts"), path.join(DIST, "fonts"));
+  fs.symlinkSync(path.join(VOLUME, "resume.pdf"), path.join(DIST, "resume.pdf"));
+  fs.symlinkSync(path.join(VOLUME, "ico"), path.join(DIST, "ico"));
+  fs.symlinkSync(path.join(VOLUME, "fonts"), path.join(DIST, "fonts"));
 
-console.log("static/ ready");
+  console.log("static/ ready");
+}
+
+main();
